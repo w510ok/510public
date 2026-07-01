25813d6740fa22afc9c5946c5f9cc652ac984605
w510ok <W510cn@gmail.com>
2026-07-01 14:26:02 +0800

    Delete siduchishui/service-worker.js

--- siduchishui/service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'sidu-chishui-sandbox-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (url.origin !== location.origin) return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => { const clone=resp.clone(); caches.open(CACHE_NAME).then(cache=>cache.put(event.request,clone)); return resp; }).catch(()=>caches.match('./index.html'))));
-});
